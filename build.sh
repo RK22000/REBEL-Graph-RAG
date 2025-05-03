1d550d4717e89eac106f5eb69fcdfd4454ebb6ba
Sindhu Satish <sindhusatish155@gmail.com>
2025-05-03 11:52:09 -0700

    build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd UI
+npm install --include=dev
+npm run build
+cd ..
+
+pip install -r requirements.txt
+
+mkdir -p static
+cp -r UI/dist/* static/
+
+uvicorn app:app --host 0.0.0.0 --port 8000
